4d50583c992538ec5fc8eba8efe6be407e4d6b2f
Chaithra Babu Reddy <BabuReddy.Chaithra@in.bosch.com>
2022-11-27 22:26:57 +0530

    update alerter.js

--- alerter.js
+++ alerter.js
@@ -1,29 +1,34 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 
 let alertFailureCount = 0;
 
-function networkAlertStub(celcius) {
+function networkAlertStub(celcius,isSuccess) {
     console.log(`Alert! Temperature is ${celcius} degrees`);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 500;
+    if (isSuccess) {
+        return 200;
+    } else {
+        return 500;
+    }
 }
 
-function alertInCelcius(farenheit) {
+/**  parameters farenheit takes numbers to be converted, isSuccess takes boolen to alert success or failure */
+function alertInCelcius(farenheit,isSuccess) {
     const celcius = (farenheit - 32) * 5 / 9;
-    const returnCode = networkAlertStub(celcius);
+    const returnCode = networkAlertStub(celcius, isSuccess);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
-alertInCelcius(400.5);
-alertInCelcius(303.6);
-expect(alertFailureCount).to.be.above(0);
+alertInCelcius(400.5,true);
+expect(alertFailureCount).equals(0);
+alertInCelcius(303.6,false);
+expect(alertFailureCount).equals(1);
+alertInCelcius(200.6, true);
+expect(alertFailureCount).equals(1);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,29 @@
-const {expect} = require('chai')
+const { expect } = require('chai');
 
+function pair() {
+    this.majorColor;
+    this.minorColor;
+    this.pairNo;
+}
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+
+    let arr = [];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
+            let p = new pair();
+            p.majorColor = majorColors[i];
+            p.minorColor = minorColors[j];
+            p.pairNo = (i * 5) + j;
+            arr.push(p);
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
         }
     }
-    return majorColors.length * minorColors.length;
+    return arr;
 }
 
 result = print_color_map();
-expect(result).equals(25);
-console.log('All is well (maybe!)');
+expect(result.length).equals(25);
+expect(result[0].majorColor).equal('White');
+console.log(result[0].majorColor);
