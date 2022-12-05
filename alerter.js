0ee47b11333321625a84000629a98e99605215cd
Chaithra <99654380+Chaithra-BabuReddy@users.noreply.github.com>
2022-12-05 19:39:07 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -1,34 +1,25 @@
 const { expect } = require('chai');
 
-let alertFailureCount = 0;
+alertFailureCount = 0;
+const threshold =37;
 
-function networkAlertStub(celcius,isSuccess) {
+function networkAlert(celcius) {
     console.log(`Alert! Temperature is ${celcius} degrees`);
-    if (isSuccess) {
+    if (celcius<) {
         return 200;
     } else {
         return 500;
     }
 }
 
-/**  parameters farenheit takes numbers to be converted, isSuccess takes boolen to alert success or failure */
-function alertInCelcius(farenheit,isSuccess) {
+function alertInCelcius(farenheit) {
     const celcius = (farenheit - 32) * 5 / 9;
-    const returnCode = networkAlertStub(celcius, isSuccess);
+    const returnCode = networkAlert(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
-
-alertInCelcius(400.5,true);
-expect(alertFailureCount).equals(0);
-alertInCelcius(303.6,false);
-expect(alertFailureCount).equals(1);
-alertInCelcius(200.6, true);
-expect(alertFailureCount).equals(1);
-console.log(`${alertFailureCount} alerts failed.`);
-console.log('All is well (maybe!)');
