10e8b39fef4859190d5529a9da9c6c98ea052820
Chaithra <99654380+Chaithra-BabuReddy@users.noreply.github.com>
2022-12-05 18:47:08 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -1,9 +1,9 @@
 const {expect} = require('chai');
 
 function size(cms) {
-    if (cms < 38) {
+    if (cms <= 38) {
         return 'S';
-    } else if (cms > 38 && cms < 42) {
+    } else if (cms > 38 && cms <= 42) {
         return 'M';
     } else {
         return 'L';
